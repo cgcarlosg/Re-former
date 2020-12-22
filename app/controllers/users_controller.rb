7023e11382a4e9473a52025cad781640017d7666
imran5644 <imi56444@gmail.com>
2020-12-23 03:42:34 +0530

    updated the forms and edit and update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,37 @@
 class UsersController < ApplicationController
 
     def New
+        @user = User.new
     end
 
-    def Create
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          redirect_to new_user_path
+        else
+          render :new
+        end
     end
 
+
+    def edit
+        # render
+        @user = User.find(params[:id])
+      end
+    
+      def update
+        # actually create in DB
+        @user = User.find(params[:id])
+    
+        if @user.update(user_params)
+          redirect_to new_user_path
+        else
+          render :edit
+        end
+      end
+      
+    private
+    def user_params 
+        params.require(:user).permit(:username, :email, :password) 
+    end 
 end
